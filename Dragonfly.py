04e8e22980b251b1af5b1f51c08495278845257c
timwei <tim960622@gmail.com>
2020-07-06 21:41:27 +0800

    [2020/07/06] add the error detection and transfer to exe file

--- Dragonfly.py
+++ Dragonfly.py
@@ -2,11 +2,46 @@ import requests
 from bs4 import BeautifulSoup
 import DataClass
 from DataClass import simplifyTableInfo
+import re
+
+
+###################################################
+ErrorID = {
+    "Login_error": -1,
+    "ID_overflow": -2
+}
+
+
+###################################################
+#\ request url
+# general ur 
+general_url = 'http://dragonfly.idv.tw/dragonfly/'
+
+# 登入
+#Login_url = 'http://dragonfly.idv.tw/dragonfly/login.php'
+Login_url = general_url + 'login.php'
+
+# 蜓種觀察資料查詢作業
+#All_Observation_Data_url = 'http://dragonfly.idv.tw/dragonfly/rec_list_view.php'
+All_Observation_Data_url = general_url + 'rec_list_view.php'
+
+# Next page url
+Next_page_url = general_url + '?pageNum_rs_dragonfly_record='
+
+# 執行簡述 url
+Brief_discriptions_url = 'view_data.php?id='
+
+# 執行詳述 url
+# http://dragonfly.idv.tw/dragonfly/read_data.php?id=64774
+Detailed_discriptions_url = 'read_data.php?id='
+
+
 
 def DataCrawler(Input_account, Input_password, Input_ID):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
     }
+    #################################################
     #\ Login account and password
     '''
     data = {
@@ -21,37 +56,15 @@ def DataCrawler(Input_account, Input_password, Input_ID):
 
 
     ###################################################
-    #\ request url
-    # general ur 
-    general_url = 'http://dragonfly.idv.tw/dragonfly/'
-
-    # 登入
-    #Login_url = 'http://dragonfly.idv.tw/dragonfly/login.php'
-    Login_url = general_url + 'login.php'
-
-    # 蜓種觀察資料查詢作業
-    #All_Observation_Data_url = 'http://dragonfly.idv.tw/dragonfly/rec_list_view.php'
-    All_Observation_Data_url = general_url + 'rec_list_view.php'
-
-    # Next page url
-    Next_page_url = general_url + '?pageNum_rs_dragonfly_record='
-
-    # 執行簡述 url
-    Brief_discriptions_url = 'view_data.php?id='
-
-    # 執行詳述 url
-    # http://dragonfly.idv.tw/dragonfly/read_data.php?id=64774
-    Detailed_discriptions_url = 'read_data.php?id='
-
-    ###################################################
     #\ 執行登入
     session = requests.Session()
-    session.post(Login_url, headers=headers, data=data)
+    Login_Response = session.post(Login_url, headers=headers, data=data)
+    
 
     #\ 執行進入"蜓種觀察資料查詢作業"
     All_Observation_Data_response = session.post(All_Observation_Data_url, headers=headers)
-    #print(response.text);
 
+    ###################################################
     #\ 下一頁
     '''
     page = 1
@@ -59,6 +72,7 @@ def DataCrawler(Input_account, Input_password, Input_ID):
     #print(response_next_page.text)
     '''
 
+    ###################################################
     #\ 執行點入簡述
     # request url = http://dragonfly.idv.tw/dragonfly/view_data.php?id=65431
     '''
@@ -67,6 +81,8 @@ def DataCrawler(Input_account, Input_password, Input_ID):
     print(response_Brief_discriptions.text)
     '''
 
+
+    ###################################################
     #\ 執行詳述
     # request url = http://dragonfly.idv.tw/dragonfly/read_data.php?id=65431
     '''
@@ -79,6 +95,7 @@ def DataCrawler(Input_account, Input_password, Input_ID):
     print('緯度 : ' + Lateral)
     '''
 
+    ###################################################
     #\ 嘗試非自己可以看的詳細內容
     '''
     id = 65430
@@ -98,6 +115,7 @@ def DataCrawler(Input_account, Input_password, Input_ID):
     '''
 
 
+    ###################################################
     #\ 解析資料
     '''
     Data_List = []
@@ -116,8 +134,25 @@ def DataCrawler(Input_account, Input_password, Input_ID):
         print(obj, sep =' ')
     '''
 
+    ###################################################
     #\ 執行GUI input
+    #\確認是否成功登入
+    soup_login_ckeck = BeautifulSoup(Login_Response.text, 'html.parser')
+    script = soup_login_ckeck.find("script").extract() # find the alert
+    alert = re.findall(r'(?<=alert\(\").+(?=\")', script.text) #\r\n    alert("登入失敗，請重新登入");\r\n  
+    if (len(alert) > 0):
+        return [ErrorID["Login_error"], ErrorID["Login_error"]] # to show the error that the password or account might be wrong
+
+    #\先確ID認是否超處範圍
     id = Input_ID
+    soup_ID_check = BeautifulSoup(All_Observation_Data_response.text, 'html.parser')
+    All_Observation_Data_response_Data_Set = soup_ID_check.find(id='theRow')
+    Max_All_Observation_Data_response_Data = All_Observation_Data_response_Data_Set.find_all('td')
+    #print('Max = ' + Max_All_Observation_Data_response_Data[0].text)
+    if (int(id) > int(Max_All_Observation_Data_response_Data[0].text) or int(id) < 0):
+        return [ErrorID["ID_overflow"], ErrorID["ID_overflow"]] # to show the error that the ID number is out of range
+
+    #\ 執行 
     response_Detailed_discriptions2 = session.post(general_url + Detailed_discriptions_url + id, headers=headers)
     soup2 = BeautifulSoup(response_Detailed_discriptions2.text, 'html.parser')
     Longitude = soup2.find(id = 'R_LNG').get('value')
@@ -125,5 +160,8 @@ def DataCrawler(Input_account, Input_password, Input_ID):
     Lateral = soup2.find(id = 'R_LAT').get('value')
     print('緯度 : ' + Lateral)
 
-
     return [Longitude, Lateral]
+
+
+
+
--- GUI.py
+++ GUI.py
@@ -1,38 +1,139 @@
 ## GUI
+# Reference:
+# https://www.geeksforgeeks.org/python-os-path-exists-method/
+# https://www.tutorialspoint.com/python/tk_message.htm
+# https://www.itread01.com/content/1548486187.html
+# https://anzeljg.github.io/rin2/book2/2405/docs/tkinter/anchors.html
+# https://www.geeksforgeeks.org/python-grid-method-in-tkinter/
 
+from Dragonfly import DataCrawler
+from ColorClass import color 
+import tkinter as tk
+from tkinter import messagebox
 from tkinter import *
 from tkinter.messagebox import *
-from Dragonfly import DataCrawler
+import os
+import os.path
 
-def show_answer():
-    [Output_LNG, Output_LAT] = DataCrawler(account.get(), password.get(), ID.get()) 
-    blank_LNG.insert(0, Output_LNG)
-    blank_LAT.insert(0, Output_LAT)
+##################################################################
+#\ arguement
+InputArgumentsLabel = ["Account", "Password", "ID"]
+
+# auto save account and password file
+path = os.getenv('temp')
+filename = os.path.join(path, 'info.txt')
+    
+##################################################################
+# check if the user enter the info properly
+def check(InputList):
+    index = 0
+    string = ""
+    for Input in InputList:
+        if (Input == ''):
+            string += "[" + InputArgumentsLabel[index] + "] "
+        index += 1
+    if (len(string) > 0):
+        messagebox.showwarning('Warning!!!', string + "should not be empty!!!!")
+    return len(string)
+
+
+#################################################################
+# 嘗試自動填寫使用者名稱和密碼
+def auto_fill():
+    try:
+        with open(filename) as fp:
+            n, p = fp.read().strip().split(',')
+            return [n, p]
+    except:
+        return['', '']
+
+
+##################################################################
+def show_result():
+    InputList = [account.get(), password.get(), ID.get()]
+
+    # check if the user enter the info properly
+    empty_or_not = check(InputList)
+
+    # this datd do not contain the Longitude and Latitude infomation
+    if (empty_or_not == 0):
+        [Output_LNG, Output_LAT] = DataCrawler(InputList[0], InputList[1], InputList[2])
+        if (Output_LNG == '' or Output_LAT == ''):
+            Output_LNG = 'No Data'
+            Output_LAT = 'No Data'
+        elif (Output_LNG == -1):    
+            messagebox.showwarning('Warning!!!', InputArgumentsLabel[0] + " or " + InputArgumentsLabel[1] + " might be incorrect!!!!")    #incorrect account or password
+        elif (Output_LNG == -2):
+            messagebox.showwarning('Warning!!!', InputArgumentsLabel[2] + " number is out of range!!!!")    #ID number overflow
+
+        blank_LNG.delete(0, END)
+        blank_LAT.delete(0, END)    
+        blank_LNG.insert(0, Output_LNG)
+        blank_LAT.insert(0, Output_LAT)
+
+    # and write the account and password to the filename
+    with open(filename, 'w') as fp:
+        fp.write(','.join((InputList[0],InputList[1])))
     
 
+################################################################### main
+################################################################### start
+# 建立一個視窗物件
 main = Tk()
-Label(main, text = "account:").grid(row=0)
-Label(main, text="password:").grid(row=1)
-Label(main, text = "ID:").grid(row=2)
-Label(main, text="The Longitude is:").grid(row=3)
-Label(main, text="The Lateral is:").grid(row=4)
 
+# 視窗基本設定
+main.title("蜻蜓資料庫經緯度查詢")
+main.geometry('380x480') # Width*Height
 
-account = Entry(main)
-password = Entry(main)
+# 設定圖片
+current_path = os.getcwd()  # directory from where script was ran
+canvas = Canvas(main, height=300, width=380)
+image_path = current_path + "\dragonfly_picture.gif"
+image_file = PhotoImage(file = image_path)
+image = canvas.create_image(0, 0, anchor='nw', image=image_file)
+canvas.grid(row = 0, column = 0, columnspan = 2)
+
+# label
+Label(main, text = "Account:").grid(row=1)
+Label(main, text = "Password:").grid(row=2)
+Label(main, text = "ID:").grid(row=3)
+Label(main, text = "The Longitude is:").grid(row=5)
+Label(main, text = "The Lateral is:").grid(row=6)
+
+# Entry
+VarName = StringVar(main, value='')
+VarPwd = StringVar(main, value='')
+account = Entry(main, textvariable = VarName)
+password = Entry(main, textvariable = VarPwd)
 ID = Entry(main)
 blank_LNG = Entry(main)
 blank_LAT = Entry(main)
 
 
-account.grid(row=0, column=1)
-password.grid(row=1, column=1)
-ID.grid(row=2, column=1)
-blank_LNG.grid(row=3, column=1)
-blank_LAT.grid(row=4, column=1)
+account.grid(row=1, column=1)
+password.grid(row=2, column=1)
+ID.grid(row=3, column=1)
+blank_LNG.grid(row=5, column=1)
+blank_LAT.grid(row=6, column=1)
 
 
-Button(main, text='Quit', command=main.destroy).grid(row=5, column=0, sticky=W, pady=4)
-Button(main, text='Enter', command=show_answer).grid(row=5, column=1, sticky=W, pady=4)
+# try to auto fill the account and password
+[n, p] = auto_fill()
+VarName.set(n)
+VarPwd.set(p)   
+
+
+# Button(main, text='Quit', command=main.destroy).grid(row=5, column=0, sticky=W, pady=4)
+Button( main,
+        text='Enter\b',
+        command=show_result,
+        height=2,
+        width=53,
+        bg="gray80").grid(row=4, column=0, columnspan=2, sticky=W, pady=4)
+        # color info : http://www.science.smith.edu/dftwiki/index.php/File:TkInterColorCharts.png
 
 mainloop()
+
+
+
+
